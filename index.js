0786a8dbb89bde279117a051dd4837efb9a05201
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2021-08-02 14:01:22 +0300

    Release 1.3.0

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const PREFIX = settings.prefix;
 if(settings["update-reminder"]) {
     request("https://raw.githubusercontent.com/OguzhanUmutlu/JSBotTemplate/main/CHANGELOG.json", {method: "GET"}, function (error, response) {
         try {
-            const CHANGELOG = JSON.parse(response.json);
+            const CHANGELOG = JSON.parse(response.body);
             const currentVersion = CHANGELOG[require("./package.json").version];
             if(!currentVersion) {
                 console.error("Current version not found in releases! Template might have been updated!");
             } else {
-                const lastVersion = Object.values(CHANGELOG)[Object.values(CHANGELOG).length];
+                const lastVersion = Object.values(CHANGELOG)[Object.values(CHANGELOG).length-1];
                 if(currentVersion.id < lastVersion.id) {
                     console.log("Template has been updated! You can download and check news in this release: " + lastVersion.release);
                 } else console.log("You are using latest Template, congratulations!");
