5471b57c20fce4059f9b97a90041385225e520e7
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2021-08-02 14:07:23 +0300

    Release 1.3.0

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,6 @@
 let Discord = require("discord.js");
 let message = new Discord.Message(new Discord.Client(), {}, new Discord.TextChannel(new Discord.Guild(new Discord.Client(), {}), {}));
+const Base = require("../Base").getInstance();
 // @CONFIG
 // @name reload
 // @description This command reloads the codes in your commands to bot!
@@ -7,7 +8,6 @@ let message = new Discord.Message(new Discord.Client(), {}, new Discord.TextChan
 // @CONFIG END
 
 const fs = require("fs");
-const Base = require("../Base").getInstance();
 
 Base.resetCommands();
 fs.readdirSync("./commands").filter(i=> i.endsWith(".js")).forEach(i=> Base.addCommand(i));
--- commands/unload.js
+++ commands/unload.js
@@ -1,13 +1,12 @@
 let Discord = require("discord.js");
 let message = new Discord.Message(new Discord.Client(), {}, new Discord.TextChannel(new Discord.Guild(new Discord.Client(), {}), {}));
 let args = [""];
+const Base = require("../Base").getInstance();
 // @CONFIG
 // @name unload
 // @description This command unloads the command for current session!
 // @idRequirement 460154149040947211
 // @CONFIG END
-
-const Base = require("../Base").getInstance();
 
 if(!args[0]) return message.reply("You should enter command name to unload!");
 if(!Base.commands[args[0]]) return message.reply("Command not found!");
