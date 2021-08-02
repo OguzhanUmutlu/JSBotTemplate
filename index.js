5f39bded17a22c6524837626b04aef89a0b79efd
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2021-08-02 13:58:32 +0300

    Update reminder and 1.3.0

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 const wio = require("wio.db");
+const request = require("request");
 const db = new wio.JsonDatabase({
     databasePath: "data.json"
 });
 const Base = require("./Base").new;
-const PREFIX = "!";
+const settings = require("./settings.json");
+const PREFIX = settings.prefix;
+if(settings["update-reminder"]) {
+    request("https://raw.githubusercontent.com/OguzhanUmutlu/JSBotTemplate/main/CHANGELOG.json", {method: "GET"}, function (error, response) {
+        try {
+            const CHANGELOG = JSON.parse(response.json);
+            const currentVersion = CHANGELOG[require("./package.json").version];
+            if(!currentVersion) {
+                console.error("Current version not found in releases! Template might have been updated!");
+            } else {
+                const lastVersion = Object.values(CHANGELOG)[Object.values(CHANGELOG).length];
+                if(currentVersion.id < lastVersion.id) {
+                    console.log("Template has been updated! You can download and check news in this release: " + lastVersion.release);
+                } else console.log("You are using latest Template, congratulations!");
+            }
+        } catch (e) {
+            console.error("An error occurred while trying to check updates.");
+        }
+    });
+}
 client.on("ready", async () => {
     client.dir = __dirname;
     Base.dir = __dirname;
